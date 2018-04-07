ab901147f34d2bd6b94e3c632a0f56779ede7d76
hurrikaani666 <hurrikaani666@gmail.com>
2018-04-07 18:11:54 +0100

    Jatkettu hieman edellisestä

--- AssetManagementWeb/AssetManagementWeb/Scripts/Applivations/Logic.js
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Applivations/Logic.js
@@ -1,6 +1,30 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
+var AssignLocationModel = /** @class */ (function () {
+    function AssignLocationModel() {
+    }
+    return AssignLocationModel;
+}());
 function initAssetAssigment() {
     $("#AssignAssetButton").click(function () {
+        var locationCode = ("#LocationCOde").valueOf();
+        var assetCode = ("#AssetCode").valueOf();
+        alert(":L " + locationCode + ", A:" + assetCode);
+        var data = new AssignLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+        $.ajax({
+            type: "POST",
+            url: "Asset/AssignLocation",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset succesfully assigned");
+                }
+                else {
+                    alert("there was an error" + data.error);
+                }
+            },
+            dataType: "json"
+        });
     });
 }
 //# sourceMappingURL=Logic.js.map
--- AssetManagementWeb/AssetManagementWeb/Scripts/Applivations/Logic.ts
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Applivations/Logic.ts
@@ -1,8 +1,35 @@
 ﻿/// <reference path="../typings/jquery/jquery.d.ts" />
 
+ class AssignLocationModel {
+    public AssetCode: string;
+    public LocationCode: string;
+    }
 
 function initAssetAssigment() {
     $("#AssignAssetButton").click(function () {
+
+        var locationCode: string = ("#LocationCOde").valueOf();
+        var assetCode: string = ("#AssetCode").valueOf();
+
+        alert(":L " + locationCode + ", A:" + assetCode);
+        var data: AssignLocationModel = new AssignLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+
+        $.ajax({
+            type: "POST",
+            url: "Asset/AssignLocation",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset succesfully assigned");
+                }
+                else {
+                    alert("there was an error" + data.error);
+                }
+            },
+            dataType: "json"
+        });
+
       
     });
 }
